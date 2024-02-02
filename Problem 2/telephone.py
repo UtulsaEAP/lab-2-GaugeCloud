5c30cff0a9ce070ee48ebd5eeea4997730fe44b0
Gauge Cloud <ggc4100@utulsa.edu>
2024-02-02 16:17:23 -0600

    Working on question 2

--- Problem 2/telephone.py
+++ Problem 2/telephone.py
@@ -1,5 +1,9 @@
 def telephone():
     phone_number = int(input())
-    ''' Type your code here. '''
+    
+    
+    print((phone_number // 10000000), end= " ")
+    print(((phone_number % 10000000) // 10000), end= "-")
+    print(phone_number % 10000)
 if __name__ == "__main__":
     telephone()
