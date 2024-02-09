0360b88a96672f4c279ca4fc1b5e8b9b6b0a4be7
Gauge Cloud <ggc4100@utulsa.edu>
2024-02-09 17:56:59 -0600

    Fixed

--- Problem 4/real_estate.py
+++ Problem 4/real_estate.py
@@ -6,6 +6,7 @@ def real_estate():
     
     print("This house is $" f'{current_price:.2f}', end= ". ")
     print("The change is $" f'{(current_price)-(last_month_price):.2f}' " since last month.")
-    print("The estimated monthly mortgage is $" f'{(current_price * 0.051)/12:.2f}', ".")
+    print("The estimated monthly mortgage is $" f'{(current_price * 0.051)/12:.2f}.')
 if __name__ == "__main__":
     real_estate()
+    
