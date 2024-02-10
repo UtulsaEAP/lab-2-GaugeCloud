ed98e93a5f6f3e15849e6d7f49739f06b0269d7f
Gauge Cloud <ggc4100@utulsa.edu>
2024-02-09 18:17:48 -0600

    Problem 3 and 4

--- Problem 3/caffeine.py
+++ Problem 3/caffeine.py
@@ -7,11 +7,11 @@ def caffeine():
     caffeine_mg_after_24 = (caffeine_mg_after_18)/2
 
     print("After 6 hours:", end=" ")
-    print(f'{caffeine_mg_after_6 :.2f}', " mg")
+    print(f'{caffeine_mg_after_6 :.2f}', "mg")
     print("After 12 hours:", end=" ")
-    print(f'{caffeine_mg_after_12 :.2f}', " mg")
+    print(f'{caffeine_mg_after_12 :.2f}', "mg")
     print("After 24 hours", end=" ")
-    print(f'{caffeine_mg_after_24 :.2f}', " mg")
+    print(f'{caffeine_mg_after_24 :.2f}', "mg")
 
     
 if __name__ == "__main__":
--- Problem 4/real_estate.py
+++ Problem 4/real_estate.py
@@ -4,9 +4,10 @@ def real_estate():
     current_price = int(input())
     last_month_price = int(input())
     
-    print("This house is $" f'{current_price:.2f}', end= ". ")
-    print("The change is $" f'{(current_price)-(last_month_price):.2f}' " since last month.")
+    print("This house is", end=" $")
+    print((current_price), end= ". ")
+    print("The change is", end=" $")
+    print((current_price)-(last_month_price), "since last month.")
     print("The estimated monthly mortgage is $" f'{(current_price * 0.051)/12:.2f}.')
 if __name__ == "__main__":
     real_estate()
-    
